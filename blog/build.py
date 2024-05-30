43218fc4e03c44e36651adef7fbd0648a4b0e844
Farbod Shahinfar <fshahinfar1@gmail.com>
2024-05-30 21:41:31 +0200

    update the blog index

--- blog/build.py
+++ blog/build.py
@@ -25,13 +25,51 @@ def build_latex():
     subprocess.check_output(cmd, shell=True, text=True)
 
 
+def read_blog_post_date(path):
+    X = "<div class='date'><span class='cmr-12'>"
+    Y = "</span></div>"
+    with open(path, 'r') as blog_file:
+        for line in blog_file:
+            if not line.startswith(X):
+                continue
+            x = len(X)
+            y = line.index(Y)
+            date = line[x:y]
+            return date
+    return '-- missing update date --'
+
+
+def read_blog_post_title(path):
+    X = len('<head><title>')
+    with open(path, 'r') as blog_file:
+        for line in blog_file:
+            if not line.startswith('<head><title>'):
+                continue
+            Y = line.index('</title>')
+            title = line[X:Y]
+            return title
+    return '-- untitled --'
+
+
 def update_blog_index(dirs):
+    css = '''
+.date-tag {
+font-size: 10pt;
+color: gray;
+padding-left: 2em;
+}
+'''
     with open('index.html', 'w') as f:
+        f.write(f'<head>\n<style>\n{css}\n</style>\n</head>\n')
+        f.write('<body>\n')
         f.write('<ol>\n')
         for dir_name in dirs:
             href=f'{dir_name}/build/blog.html'
-            f.write(f'<oi><a href={href}>{dir_name}</a></oi>\n')
+            title = read_blog_post_title(href)
+            date = read_blog_post_date(href)
+            f.write(f'<li><a href={href} target="_blank">{title}</a><span class="date-tag">{date}</span></li>\n')
         f.write('</ol>\n')
+        f.write('</body>')
 
 
 def main():
