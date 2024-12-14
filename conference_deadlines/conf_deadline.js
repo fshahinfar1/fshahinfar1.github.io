0d461250e02cd1e8e70e33d89927cb1e1b8bfa2a
Farbod Shahinfar <fshahinfar1@gmail.com>
2024-12-14 23:19:24 +0100

    update conf_deadline script

--- conference_deadlines/conf_deadline.js
+++ conference_deadlines/conf_deadline.js
@@ -6,9 +6,21 @@ function on_document_load() {
     const tr = trs[i];
     const deadline = tr.children[1].textContent;
     const splits = deadline.split('/');
+    if (splits.length != 3) {
+      continue;
+    }
     const y = Number.parseInt(splits[2]);
     const monIndex = Number.parseInt(splits[1]) - 1;
     const day = Number.parseInt(splits[0]);
+    if (y < 2000 || y > 4000) {
+      continue
+    }
+    if (monIndex < 0 || monIndex > 11) {
+      continue
+    }
+    if (day < 1 || day > 31) {
+      continue
+    }
     const d = new Date(y, monIndex, day);
     const delta = d - now;
     const remaining_days = Math.floor(delta / 1000 / 60 / 60 / 24);
